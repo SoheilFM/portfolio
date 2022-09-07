d11e367d5db6c56c9512f70854344e9bf9586901
Soheil <soheil.farid1999@yahoo.com>
2022-09-07 13:22:08 +0430

    Edit Navbar and Add Second Theme  Switch

--- src/data/themeData.js
+++ src/data/themeData.js
@@ -1,11 +1,13 @@
 /* eslint-disable */
+import { ListItemSecondaryAction } from '@material-ui/core'
 import { 
     greenThemeLight, greenThemeDark, bwThemeLight, bwThemeDark, blueThemeLight, blueThemeDark, redThemeLight, redThemeDark, orangeThemeLight, orangeThemeDark, purpleThemeLight, purpleThemeDark, pinkThemeLight, pinkThemeDark, yellowThemeLight, yellowThemeDark
 } from '../theme/theme'
 
 
 export const themeData = {
-    theme: blueThemeDark
+    defaultTheme: blueThemeDark,
+    secondaryTheme: blueThemeLight
 }
 
 
