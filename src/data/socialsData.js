031f90c2486c39679a60cda5f769b9725973d3c1
Soheil <soheil.farid1999@yahoo.com>
2022-09-08 15:45:31 +0430

    Edit github link

--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -1,7 +1,7 @@
 export const socialsData = {
     github: 'https://github.com/SoheilFM',
     //facebook: 'https://www.facebook.com/',
-    linkedIn: 'https://www.linkedin.com/soheil-faridmohammadzadegan',
+    linkedIn: 'https://www.linkedin.com/in/soheil-faridmohammadzadegan-2a0b49234',
     //instagram: 'https://www.instagram.com/',
     codepen: 'https://codepen.io/soheilfm',
     //twitter: 'https://twitter.com/',
